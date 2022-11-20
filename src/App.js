949a7bbe6bab03f124079b058eae7e33ff65cd76
Shaima <shaima.anvar@gmail.com>
2022-11-20 16:38:50 -0500

    added maincomp and rendered it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 //import logo from './logo.svg';
 import './App.css';
+import MainComp from './MainComp';
 
 function App() {
   return (
     <div className="App">
       
         <h2>Hello world</h2>
+        <MainComp/>
     </div>
   );
 }
