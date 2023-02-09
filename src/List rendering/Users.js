ce171db092e29087d7d84cbec005cdb74054ee9b
Shaima Anvar <shaima.anvar@gmail.com>
2023-02-09 17:39:28 -0500

    printing the users in asc order

--- src/List rendering/Users.js
+++ src/List rendering/Users.js
@@ -3,33 +3,40 @@ import React, { Component } from 'react'
 class Users extends Component {
     constructor(props){
         super(props);
-        //this.userName= ["A","B","c",]
-        this.userName=[
-            {
-                name:"AA",
-                id:8
-            },
-            {
-                name:"BB",
-                id:6
-            },
-            {
-                name:"CC",
-                id:10
-            },
-        ]
-        
+        this.state={
+            userName:[
+                {
+                    name:"CC",
+                    id:8
+                },
+                {
+                    name:"BB",
+                    id:6
+                },
+                {
+                    name:"AA",
+                    id:10
+                },
+            ],  
+        } 
+    }
+    handleSort=()=>{
+        let sorted=this.state.userName.sort((user1,user2)=>user1.id-user2.id);
+        console.log(sorted);
+        this.setState(
+            {userName:sorted}
+        )
     }
   render() {
     return (
       <div> 
-        <button>Sort Ascending</button>
+        <button onClick={this.handleSort}>Sort Ascending</button>
         <table>
             <tr>
                 <th>Name</th>
                 <th>Id</th>
             </tr>
-            {this.userName.map((user,index)=>(
+            {this.state.userName.map((user,index)=>(
                 <tr>
                     <td>{user.name}</td>
                     <td>{user.id}</td>
